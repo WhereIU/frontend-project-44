4d98363448371f2cecfb0b51de69d643524f9af0
Saint <146729423+WhereIU@users.noreply.github.com>
2024-01-29 14:15:18 +0300

    Update get-gcd.js

--- src/helpers/get-gcd.js
+++ src/helpers/get-gcd.js
@@ -1,4 +1,8 @@
 export default (firstNum, secondNum) => {
+  if (firstNum === secondNum) {
+    return firstNum;
+  }
+
   const numbers = []; // going to be [min, max]
   if (firstNum < secondNum) {
     numbers.push(firstNum);
@@ -7,11 +11,9 @@ export default (firstNum, secondNum) => {
     numbers.push(secondNum);
     numbers.push(firstNum);
   }
-
   if (numbers[1] % numbers[0] === 0) {
     return numbers[0]; // minNum is gcd of maxNum
   }
-
   let possibleDivisor = Math.floor(numbers[0] / 2); // in first half of minNum
   for (const minCheck = 2; possibleDivisor >= minCheck; possibleDivisor -= 1) {
     // gcd should be in range 2 <= minNum/2 of check
