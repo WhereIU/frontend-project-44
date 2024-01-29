f9601720c9609b32cd8835c2046e52a305d3b2ef
Saint <146729423+WhereIU@users.noreply.github.com>
2024-01-29 14:16:11 +0300

    Update is-prime.js

--- src/helpers/is-prime.js
+++ src/helpers/is-prime.js
@@ -1,4 +1,7 @@
-const isPrime = (number) => {
+export default (number) => {
+  if (number < 2) {
+    return false;
+  }
   const halfOfNumber = Math.floor(number / 2);
   for (let divider = 2; divider <= halfOfNumber; divider += 1) {
     if (number / divider === Math.floor(number / divider)) {
